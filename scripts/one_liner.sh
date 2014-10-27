41847474d13870eb7fec3983308204b087c0a736
Brian Rackle <brianrackle@hotmail.com>
2014-10-26 19:22:52 -0700

    Update one_liner.sh

--- scripts/one_liner.sh
+++ scripts/one_liner.sh
@@ -2,39 +2,34 @@
 ## Initialize Environment
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y g++ subversion cmake emacs git xdg-utils htop ncurses-dev ruby ruby-dev nodejs make default-jre default-jdk ack-grep gnuplot-x11 lldb-3.5 zram-config
+sudo apt-get install -y g++ subversion cmake emacs git xdg-utils htop ncurses-dev ruby ruby-dev nodejs make default-jre default-jdk ack-grep gnuplot-x11 zram-config
 sudo mkdir repos
 
-#DISCARD START REPLACE WITH apt-get install clang-3.5 lldb-3.5
 ## Pull Clang and LLVM
 cd ~/repos/
-sudo mkdir clang
-cd clang
+sudo mkdir llvm_src
+cd llvm_src
 sudo svn co http://llvm.org/svn/llvm-project/llvm/branches/release_35/ llvm
 cd llvm/tools
 sudo svn co http://llvm.org/svn/llvm-project/cfe/branches/release_35/ clang
+sudo svn co http://llvm.org/svn/llvm-project/lldb/branches/release_35/ lldb
 
 ## Build Clang and LLVM
-cd ~/repos/clang
-sudo mkdir build
-cd build
+cd ~/repos/llvm_src
+sudo mkdir build_llvm
+cd build_llvm
 sudo ../llvm/configure --enable-optimized --enable-targets=host --disable-compiler-version-checks
-sudo make -j 8
+sudo make -j 4
 sudo make install
 #DISCARD END
 
-#Create .bash_aliases file
-#alias clang='clang-3.5'
-#alias clang++?
-#alias lldb='lldb-3.5'
-
 ## Pull and Make libc++
-cd ~/repos/clang
+cd ~/repos/llvm_src
 sudo svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
 sudo mkdir build_libcxx
 cd build_libcxx
 sudo CC=clang CXX=clang++ cmake -G "Unix Makefiles" -DLIBCXX_CXX_ABI=libsupc++ -DLIBCXX_LIBSUPCXX_INCLUDE_PATHS="/usr/include/c++/4.8;/usr/include/x86_64-linux-gnu/c++/4.8" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr $HOME/repos/clang/libcxx
-sudo make -j 8
+sudo make -j 4
 sudo make install
 
 ## Setup Emacs
