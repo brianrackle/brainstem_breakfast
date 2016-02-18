56c92b1f0e697d4acb2f1a49dbc00752262e8044
Brian Rackle <brianrackle@hotmail.com>
2016-02-18 12:32:24 -0800

    Update one_liner.sh

--- scripts/one_liner.sh
+++ scripts/one_liner.sh
@@ -32,7 +32,10 @@ sudo -u postgres psql postgres
 sudo apt-get install openjdk-8-jre-headless
 wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.2.0/elasticsearch-2.2.0.deb
 sudo dpkg -i elasticsearch-2.2.0.deb
-
+sudo update-rc.d elasticsearch defaults 95 10
+### In order to start elasticsearch, execute
+#sudo /etc/init.d/elasticsearch start
+ 
 ##clone repos
 mkdir repos
 cd ~/repos/
