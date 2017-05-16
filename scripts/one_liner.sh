1dc1c79b27da9b03e9d50a707d7a1540f668c16b
Brian Rackle <brianrackle@hotmail.com>
2017-05-16 14:11:09 -0700

    Update one_liner.sh

--- scripts/one_liner.sh
+++ scripts/one_liner.sh
@@ -11,7 +11,8 @@ wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
 tar xf Python-3.6.1.tar.xz
 cd Python-3.6.1
 ./configure --prefix=/usr/local --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib" --enable-optimizations
-make && make altinstall
+make
+sudo make altinstall
 
 #postgres
 sudo apt-get install postgresql-9.4
