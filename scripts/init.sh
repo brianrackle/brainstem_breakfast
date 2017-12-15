87bb63c5be0245a3e7c0ac06101ffa161227210b
Brian Rackle <brianrackle@hotmail.com>
2017-12-15 13:02:14 -0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,6 @@
 ## ./init.sh
 
 read -s -p "password: " sudoPW
-echo $sudoPW | sudo -S yum update
-
 echo $sudoPW | sudo -S apt update -y
 echo $sudoPW | sudo -S apt upgrade -y
 echo $sudoPW | sudo -S apt install -y npm python3-dev libffi-dev libssl-dev update-manager terminator subversion emacs htop make ack-grep g++ cmake git curl automake python3-pip open-vm-tools open-vm-tools-desktop
