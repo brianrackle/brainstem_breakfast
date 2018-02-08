6dadefa29de58e061eb47c5d21a3f4fb41126fb3
Brian Rackle <brianrackle@hotmail.com>
2018-02-08 13:59:49 -0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,7 +7,7 @@
 read -s -p "password: " sudoPW
 echo $sudoPW | sudo -S apt update -y
 echo $sudoPW | sudo -S apt upgrade -y
-echo $sudoPW | sudo -S apt install -y gnome-tweak-tool nodejs npm python3-dev libffi-dev libssl-dev update-manager terminator subversion emacs htop make g++ cmake git curl automake python3-pip open-vm-tools open-vm-tools-desktop
+echo $sudoPW | sudo -S apt install -y gnome-tweak-tool gnome-shell-extensions nodejs npm python3-dev libffi-dev libssl-dev update-manager terminator subversion emacs htop make g++ cmake git curl automake python3-pip open-vm-tools open-vm-tools-desktop
 
 #nvm
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
