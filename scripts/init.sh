1765a2f9811575e2bc87af644f67845ef12f2ff8
Brian Rackle <brianrackle@hotmail.com>
2018-02-08 10:41:05 -0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,7 +7,7 @@
 read -s -p "password: " sudoPW
 echo $sudoPW | sudo -S apt update -y
 echo $sudoPW | sudo -S apt upgrade -y
-echo $sudoPW | sudo -S apt install -y npm python3-dev libffi-dev libssl-dev update-manager terminator subversion emacs htop make ack-grep g++ cmake git curl automake python3-pip open-vm-tools open-vm-tools-desktop
+echo $sudoPW | sudo -S apt install -y npm python3-dev libffi-dev libssl-dev update-manager terminator subversion emacs htop make g++ cmake git curl automake python3-pip open-vm-tools open-vm-tools-desktop
 
 #virtualenv
 #http://docs.python-guide.org/en/latest/dev/virtualenvs/
