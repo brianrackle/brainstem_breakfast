8810ef692b8c24fac7b9bb019ac115b0e49d9611
Brian Rackle <brianrackle@hotmail.com>
2018-02-08 13:47:19 -0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,13 +7,10 @@
 read -s -p "password: " sudoPW
 echo $sudoPW | sudo -S apt update -y
 echo $sudoPW | sudo -S apt upgrade -y
-echo $sudoPW | sudo -S apt install -y npm python3-dev libffi-dev libssl-dev update-manager terminator subversion emacs htop make g++ cmake git curl automake python3-pip open-vm-tools open-vm-tools-desktop
+echo $sudoPW | sudo -S apt install -y nodejs npm python3-dev libffi-dev libssl-dev update-manager terminator subversion emacs htop make g++ cmake git curl automake python3-pip open-vm-tools open-vm-tools-desktop
+
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 #virtualenv
 #http://docs.python-guide.org/en/latest/dev/virtualenvs/
 pip3 install virtualenv
-
-sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
-
-npm install n --global
-sudo n latest
