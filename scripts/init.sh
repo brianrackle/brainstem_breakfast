bde77c586d61589a54a8a280a9a2663103909cbe
Brian Rackle <brianrackle@hotmail.com>
2018-02-08 17:25:19 -0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,7 +7,7 @@
 read -s -p "password: " sudoPW
 echo $sudoPW | sudo -S apt update -y
 echo $sudoPW | sudo -S apt upgrade -y
-echo $sudoPW | sudo -S apt install -y gnome-tweak-tool gnome-shell-extensions nodejs npm python3-dev libffi-dev libssl-dev update-manager terminator subversion emacs htop make g++ cmake git curl automake python3-pip open-vm-tools open-vm-tools-desktop
+echo $sudoPW | sudo -S apt install -y gnome-tweak-tool gnome-shell-extensions gnome-session nodejs npm python3-dev libffi-dev libssl-dev update-manager terminator subversion emacs htop make g++ cmake git curl automake python3-pip open-vm-tools open-vm-tools-desktop
 
 #nvm
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
@@ -15,3 +15,7 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | 
 #virtualenv
 #http://docs.python-guide.org/en/latest/dev/virtualenvs/
 pip3 install virtualenv
+
+#reason
+npm install -g https://github.com/reasonml/reason-cli/archive/3.0.4-bin-linux.tar.gz
+
